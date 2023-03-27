0b2fbade53c9ee35b60104379a19decbfbf0b583
alexander.oleksenko <vasyllisnyimk@gmail.com>
2023-03-27 16:13:20 +0300

    base settings

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,16 @@
+// Components
+import PageWrapper from 'components/PageWrapper/PageWrapper';
+
+// Styles
+import s from './Homepage.module.scss';
+
+const HomePage = () => {
+  return (
+    <PageWrapper>
+      <div>Add Product</div>
+      Add Product
+    </PageWrapper>
+  );
+};
+
+export default HomePage;
