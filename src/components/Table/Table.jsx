59b0c92a46d08e01d7305e8ba27af0f4c30f3d2d
vasyllisnyi <vasyllisnyimk@gmail.com>
2023-03-27 17:28:08 +0300

    finish

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -1,34 +1,38 @@
-import React from 'react';
+// Styles
+import s from './Table.module.scss';
 
+// API
 import tableData from 'api/api';
 
 export default function Table() {
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Protocol</th>
-          <th>Port</th>
-          <th>Rule</th>
-          <th>Attached Groups</th>
-          <th>Status</th>
-        </tr>
-      </thead>
-      <tbody>
-        {tableData.map(item => {
-          return (
-            <tr key={item.id}>
-              <td>{item.name}</td>
-              <td>{item.protocol}</td>
-              <td>{item.port}</td>
-              <td>{item.rule}</td>
-              <td>{item.attachedGroups}</td>
-              <td>{item.status}</td>
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
+    <>
+      <table className={s.table}>
+        <thead className={s.table_head}>
+          <tr>
+            <th className={s.title}>Name</th>
+            <th className={s.title}>Protocol</th>
+            <th className={s.title}>Port</th>
+            <th className={s.title}>Rule</th>
+            <th className={s.title}>Attached Groups</th>
+            <th className={s.title}>Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          {tableData.map(item => {
+            return (
+              <tr key={item.id}>
+                <td className={s.item}>{item.name}</td>
+                <td className={s.item}>{item.protocol}</td>
+                <td className={s.item}>{item.port}</td>
+                <td className={s.item}>{item.rule}</td>
+                <td className={s.item}>{item.attachedGroups}</td>
+                <td className={s.item}>{item.status}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </>
   );
 }
